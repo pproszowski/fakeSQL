f712bb8b69946eaaece53cc61e4d5b80ad272ced
Wasyl98 <m.wasiluk98@gmail.com>
2018-04-22 15:51:23 +0200

    First few addons

--- src/Table.java
+++ src/Table.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
+    private List<Column> columns;
+    private List<Record> records;
+    private Table select()
+
+
+    public Table(){
+        columns = new ArrayList<>();
+        records = new ArrayList<>();
+    }
+
 }
+
+
