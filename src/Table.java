d59995d92aeac2e1b1f8dbefe0bbe8f4cd3584e2
Wasyl98 <m.wasiluk98@gmail.com>
2018-04-22 16:05:46 +0200

    Next changes

--- src/Table.java
+++ src/Table.java
@@ -2,15 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
+    private String name;
     private List<Column> columns;
     private List<Record> records;
-    private Table select()
-
 
     public Table(){
-        columns = new ArrayList<>();
-        records = new ArrayList<>();
+        this.name = name;
+        this.columns = new ArrayList<>();
+        this.records = new ArrayList<>();
     }
+
+    private Table select(List<String>, whichColumns) {
+
+    }
+
+
+
 
 }
 
