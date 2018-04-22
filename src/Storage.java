7983039910f29a1675272c646e0b5347df03b0c4
Piotr Proszowski <215165@edu.p.lodz.pl>
2018-04-22 16:07:26 +0200

    Added Storage definition ad query class

--- src/Storage.java
+++ src/Storage.java
@@ -1,5 +1,28 @@
-public class Storage {
-    public static void main(String[] args){
+import java.util.List;
 
+public class Storage {
+    private String name;
+    private List<Database> databases;
+    private Database currentDatabase;
+
+    public Storage(String _name){
+        name = _name;
     }
+
+    private boolean addTable(Table table){
+        return true;
+    }
+
+    private boolean deleteTable(String whichTable){
+        return true;
+    }
+
+    public Response executeQuery(Query query){
+        return new Response();
+    }
+
+    public int howManyDatabases(){
+        return databases.size();
+    }
+
 }
